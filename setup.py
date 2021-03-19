d83b559520388a3b2b9b5e1e8cc9920490b309e0
caifh <caifh@txxy.com>
2021-03-19 11:40:27 +0800

    updated creditutils version with 0.2.4.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='creditutils',
-    version='0.2.3',
+    version='0.2.4',
     author='liuzhy',
     author_email='liuzhy@tianxiaxinyong.com',
     description='python small tools util.',
